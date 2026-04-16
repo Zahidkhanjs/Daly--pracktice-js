2e877e8dd420e6e414becfee7455401ab4e33d79
zahidkhanjs <zahidshahwani9925@gmail.com>
2026-04-16 07:10:11 +0500

    my first commit

---
+++ Introduction/app.js
@@ -0,0 +1,58 @@
+// Debugging    //data ka bags nikalna 
+
+// console.log("zahidkhan");
+
+//data typs 
+// string  "zahid khan"
+// number  123 .12
+// boolean  true fals
+
+// symbol 
+// bignit
+// undefine 
+// nill 
+// object 
+
+
+
+// data typs 
+//string "zahid khan"
+// Number 234/234
+//boolean  true fals 
+
+
+// object 
+// undefine 
+// symbol 
+// bigint
+// nill
+
+
+//  console.log("zahid khan ");
+//  console.log(234);
+//  console.log(true );
+
+
+//  console.log("zahid khan ");
+//  console.log(23423);
+//  console.log(false);
+
+
+
+// ()  function 
+// alert ("hello world ")
+// alert ( "hello world 2")
+// alert ( "hello world 3")
+
+// varible mean yeak khali box jub bie data chie to iss sy wo data 
+// bola sakty hooo 
+
+// var username  = prompt("enter your name")
+
+var firstname = "zahid khan"
+var lastname = "sabir khan"
+var age = 23
+
+var firstname = prompt("Enter your name ")
+var age = prompt( "Type your ange ")
+var lastname =prompt ("Enter your last name ")
