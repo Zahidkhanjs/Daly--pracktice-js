fbae2b54f09ef90951883cbef751391d332d4a65
zahidkhanjs <zahidshahwani9925@gmail.com>
2026-04-18 17:00:53 +0500

    first commit

--- Introduction/app.js
+++ Introduction/app.js
@@ -49,13 +49,13 @@
 
 // var username  = prompt("enter your name")
 
-var firstname = "zahid khan"
-var lastname = "sabir khan"
-var age = 23
+// var firstname = "zahid khan"
+// var lastname = "sabir khan"
+// var age = 23
 
-var firstname = prompt("Enter your name ")
-var age = prompt( "Type your ange ")
-var lastname =prompt ("Enter your last name ")
+// var firstname = prompt("Enter your name ")
+// var age = prompt( "Type your ange ")
+// var lastname =prompt ("Enter your last name ")
 
 
 
@@ -65,9 +65,72 @@ var lastname =prompt ("Enter your last name ")
 
 
 
-var username = ("zahid khan")
-console.log(username);  // initlize  kia 
-username =("sabirkhan")  // reassigne  kia 
-var username = ( "sabir khan shahwani ")
+// var username = ("zahid khan")
+// console.log(username);  // initlize  kia 
+// username =("sabirkhan")  // reassigne  kia 
+// var username = ( "sabir khan shahwani ")
 
 
+//debadding
+ 
+// console.log("hello world ");
+
+
+// var firstuser = "zahid khan"  //initlization
+// firstuser = "zahid khan shahwani" // initlization 
+// var 
+
+
+
+// var username = "zahidkhan"  // initlization
+// username ="zahidkhanshahwani" // decliration
+// var username = "username khan "  // redecliration
+
+// Data typs in javascriopt
+   // string 
+   // number 
+   // boolean
+
+   //symbole 
+   // bignit
+   //undefine nill
+
+
+//    var username = prompt ( "PLEAS ENTER YOUR NAME ")
+//    var classes = prompt ("  WITCH CLASS DO YOU READ ")
+//    var subject = prompt( "  ENTER SYBJECT NAME ")
+//    var mobile = prompt("MOBILE NUMBER")
+//    var school = prompt ("ENTER YOUR SCHOOL NAME ")
+
+//    console.log("username " + username + "witch class" + classes + "subject name " + subject + "mobile number is " + mobile + "witch school" + school );
+
+
+
+
+// round methords
+
+
+Math.round //   nearest value
+Math.round // nearest value
+Math.round // nearest value
+Math.round // nearest vlae
+Math.round // nearest vale
+Math.round // nearest vale
+Math.round // nearest vale
+
+
+Math.floor // down 
+Math.floor // down 
+Math.floor // down
+Math.floor // down
+Math.floor // down
+Math.floor // down
+Math.floor // down
+Math.floor // down
+
+
+
+Math.ceil // up
+Math.ceil // up
+Math.ceil // up
+Math.ceil // u
